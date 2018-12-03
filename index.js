7d7a8f7e89a8c0eb87a22dea64c2c33f47b593c3
edo <achmad.ridho@amarbank.co.id>
2018-12-03 18:23:55 +0700

    a

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ const app = express()
 const port = 3001
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/test',test)
 
+async function test(req,res,next){
+    console.log(req.body)
+    console.log("disini")
+    res.send("hai")
+}
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
